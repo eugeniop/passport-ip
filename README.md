5b8dc47d507d626bbb83c58d63f0fc7d32df8d4c
José F. Romaniello <jfromaniello@gmail.com>
2015-08-18 14:45:46 -0300

    use req.ip instead of X-Forwarded-For

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-passport-ip
-===========
+An IP based authentication passport strategy.
 
-An IP based authentication passport strategy
+## Installation
+
+npm i eugeniop/passport-ip --save
+
+## Usage
+
+Register the strategy in passport as follows:
+
+```javascript
+var passport = require('passport');
+var IpStrategy = require('passport-ip').Strategy;
+
+passport.use(new IpStrategy({
+  range: '10.0.0.0/8'
+}, function(profile, done){
+  done(null, profile);
+  //profile.id is the ip address.
+}));
+```
+
+## Configuration behind a proxy
+
+If you are running your express application behind a proxy that you trust:
+
+```javascript
+app.enable('trust proxy');
+```
+
+Then passport-ip will use the X-Forwarded-For header.
+
+## License
+
+MIT 2014 Eugenio Pace
--- test/strategy-test.js
+++ test/strategy-test.js
@@ -6,17 +6,17 @@ var BadRequestError = require('passport-ip/errors/badrequesterror');
 
 
 vows.describe('ipStrategy').addBatch({
-  
+
   'strategy': {
     topic: function() {
       return new Strategy({range:'1.1.1.1'}, function(){});
     },
-    
+
     'should be named session': function (strategy) {
       assert.equal(strategy.name, 'ip');
     },
   },
-  
+
   'strategy handling a request in range': {
     topic: function() {
       var strategy = new Strategy({range:'1.1.1.1/5'}, function(){});
@@ -26,13 +26,13 @@ vows.describe('ipStrategy').addBatch({
           assert.equal(url, '/login/callback?code=dummy');
           strategy.authenticate( {
                                   query: { code: 'dummy' },
-                                  headers: {'x-forwarded-for': '1.1.1.1'}
+                                  ip: '1.1.1.1'
                                  });
       };
 
       return strategy;
     },
-    
+
     'after augmenting with actions': {
       topic: function(strategy) {
         var self = this;
@@ -42,21 +42,21 @@ vows.describe('ipStrategy').addBatch({
 
         strategy.success = function(user) {
           self.callback(null, user);
-        }
+        };
 
         strategy.fail = function() {
           self.callback(new Error('should-not-be-called'));
-        }
-        
+        };
+
         strategy._verify = function(client, done) {
           done(null, { user_id: client.id });
-        }
-        
+        };
+
         process.nextTick(function () {
           strategy.authenticate(req);
         });
       },
-      
+
       'should not generate an error' : function(err, user) {
         assert.isNull(err);
       },
@@ -66,42 +66,47 @@ vows.describe('ipStrategy').addBatch({
     },
   },
 
-    'strategy handling a request in multiple ranges': {
+  'strategy handling a request in multiple ranges': {
     topic: function() {
-      var strategy = new Strategy({range:'1.1.1.1/5,10.0.0.0/10'.split(",")}, function(){});
+      var strategy = new Strategy({
+        range: ['1.1.1.1/5' , '10.0.0.0/10']
+      }, function(err, profile, done) {
+
+      });
 
       //Mock
       strategy.redirect = function(url) {
         assert.equal(url, '/login/callback?code=dummy');
-        strategy.authenticate( {
-                                  query: { code: 'dummy' },
-                                  headers: {'x-forwarded-for': '1.1.1.1'}
-                                 });
+        strategy.authenticate({ query: { code: 'dummy' }, ip: '1.1.1.1' });
         return strategy;
       };
+
+      return strategy;
     },
-    
+
     'after augmenting with actions': {
       topic: function(strategy) {
         var self = this;
+        var req = {};
 
         strategy.success = function(user) {
           self.callback(null, user);
-        }
+        };
 
         strategy.fail = function() {
-          self.callback(new Error('should-not-be-called'));
-        }
-        
+          throw new Error('should-not-be-called');
+        };
+
         strategy._verify = function(client, done) {
           done(null, { user_id: client.id });
-        }
-        
+        };
+
         process.nextTick(function () {
           strategy.authenticate(req);
         });
+
       },
-      
+
       'should not generate an error' : function(err, user) {
         assert.isNull(err);
       },
@@ -113,18 +118,23 @@ vows.describe('ipStrategy').addBatch({
 
   'strategy handling a request in multiple ranges with an optional username': {
     topic: function() {
-      var strategy = new Strategy({range:'1.1.1.1/5,10.0.0.0/10'.split(","), username: '123'}, function(){});
+      var strategy = new Strategy({
+        range: ['1.1.1.1/5', '10.0.0.0/10'],
+        username: '123'
+      }, function(){});
 
       //Mock
       strategy.redirect = function(url) {
-          assert.equal(url, '/login/callback?code=dummy');
-          strategy.authenticate( {
-                                  query: { code: 'dummy' },
-                                  headers: {'x-forwarded-for': '1.1.1.1'}
-                                 });
+        assert.equal(url, '/login/callback?code=dummy');
+        strategy.authenticate({
+                                query: { code: 'dummy' },
+                                ip: '1.1.1.1'
+                               });
 
-          return strategy;
+        return strategy;
       };
+
+      return strategy;
     },
 
     'after augmenting with actions': {
@@ -134,22 +144,23 @@ vows.describe('ipStrategy').addBatch({
 
         strategy.success = function(user) {
           self.callback(null, user);
-        }
+        };
 
         strategy.fail = function() {
           self.callback(new Error('should-not-be-called'));
-        }
-        
+        };
+
         strategy._verify = function(client, done) {
           done(null, { user_id: client.id, username: client.username });
-        }
-        
-        req.headers = { 'x-forwarded-for': '1.1.1.1'};
+        };
+
+        req.ip = '1.1.1.1';
+
         process.nextTick(function () {
           strategy.authenticate(req);
         });
       },
-      
+
       'should not generate an error' : function(err, user) {
         assert.isNull(err);
       },
@@ -159,7 +170,7 @@ vows.describe('ipStrategy').addBatch({
       },
     },
   },
-  
+
   'strategy handling a request off range fails': {
     topic: function() {
       var strategy = new Strategy({range:'1.1.1.1/5'}, function(){});
@@ -168,79 +179,81 @@ vows.describe('ipStrategy').addBatch({
           assert.equal(url, '/login/callback?code=dummy');
           strategy.authenticate( {
                                   query: { code: 'dummy' },
-                                  headers: {'x-forwarded-for': '192.168.1.6'}
+                                  ip: '192.168.1.6'
                                  });
       };
 
       return strategy;
     },
-    
+
     'after augmenting with actions': {
       topic: function(strategy) {
         var self = this;
         var req = {};
         strategy.success = function(user) {
           self.callback(new Error('should-not-be-called'));
-        }
+        };
         strategy.fail = function() {
           self.callback(null);
-        }
-        
+        };
+
         strategy._verify = function(client, done) {
           self.callback(new Error('should-not-be-called'));
-        }
-        
+        };
+
         process.nextTick(function () {
           strategy.authenticate(req);
         });
       },
-      
+
       'should not generate an error' : function(err, user) {
         assert.isNull(err);
       },
       'should authenticate' : function(err, user) {
-         
+
       },
     },
   },
-  
+
   'strategy handling a request that encounters an error during verification': {
     topic: function() {
       var strategy = new Strategy({range: '1.1.1.1/2'}, function(){});
-      
+
       strategy.redirect = function(url) {
           assert.equal(url, '/login/callback?code=dummy');
           strategy.authenticate( {
                                   query: { code: 'dummy' },
-                                  headers: {'x-forwarded-for': '1.1.1.1'}
+                                  ip: '1.1.1.1'
                                  });
         return strategy;
       };
+
+      return strategy;
     },
-    
+
     'after augmenting with actions': {
       topic: function(strategy) {
         var self = this;
         var req = {};
         strategy.success = function(user) {
           self.callback(new Error('should-not-be-called'));
-        }
+        };
         strategy.fail = function() {
           self.callback(new Error('should-not-be-called'));
-        }
+        };
         strategy.error = function(err) {
           self.callback(null, err);
-        }
-        
+        };
+
         strategy._verify = function(client, done) {
           done(new Error('something-went-wrong'));
-        }
-        
+        };
+
         process.nextTick(function () {
           strategy.authenticate(req);
         });
       },
-      
+
       'should not call success or fail' : function(err, e) {
         assert.isNull(err);
       },
@@ -256,27 +269,27 @@ vows.describe('ipStrategy').addBatch({
 
       strategy.redirect = function(url) {
         assert.equal(url, '/login/callback?code=dummy');
-        strategy.authenticate( {
-                                  query: { code: 'dummy' },
-                                 });
+        strategy.authenticate( { query: { code: 'dummy' } });
         return strategy;
       };
+
+      return strategy;
     },
-    
+
     'after augmenting with actions': {
       topic: function(strategy) {
         var self = this;
         var req = {};
         strategy.fail = function(info) {
           self.callback(null, info);
-        }
-        
+        };
+
         req.headers =  {} ;
         process.nextTick(function () {
           strategy.authenticate(req);
         });
       },
-      
+
       'should fail authentication' : function(err) {
         // fail action was called, resulting in test callback
         assert.isTrue(true);
@@ -287,7 +300,7 @@ vows.describe('ipStrategy').addBatch({
       },
     },
   },
-  
+
   'strategy constructed without a verify callback': {
     'should throw an error': function (strategy) {
       assert.throws(function() { new Strategy() });
